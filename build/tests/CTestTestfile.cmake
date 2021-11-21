032557e73af5e5e6360085844ccd9e61031c94cc
yyun <xuyunfeng723@163.com>
2021-11-21 22:40:43 +0800

    modify vscode config

---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/euweb/mygit/gamma-ray/tests
+# Build directory: /home/euweb/mygit/gamma-ray/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test("engine.unit_tests" "/home/euweb/mygit/gamma-ray/build/unit_tests")
+set_tests_properties("engine.unit_tests" PROPERTIES  _BACKTRACE_TRIPLES "/home/euweb/mygit/gamma-ray/tests/CMakeLists.txt;35;add_test;/home/euweb/mygit/gamma-ray/tests/CMakeLists.txt;0;")
